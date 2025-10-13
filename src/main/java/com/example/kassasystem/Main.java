882491bf921ad67718c6393aedd075c388bc150c
ivar01nohre <nohreivar@gmail.com>
2025-10-13 16:17:50 +0200

    test

--- src/main/java/com/example/kassasystem/Main.java
+++ src/main/java/com/example/kassasystem/Main.java
@@ -3,5 +3,6 @@ package com.example.kassasystem;
 public class Main {
     public static void main(String[] args) {
         System.out.println("goodbye!");
+        System.out.println("test!");
     }
 }
