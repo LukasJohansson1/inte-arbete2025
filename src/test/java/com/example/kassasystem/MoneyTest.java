3b00edc4304bf71d73f8429453d916928212cb02
ivar01nohre <nohreivar@gmail.com>
2025-10-14 13:41:08 +0200

    tests for money

--- src/test/java/com/example/kassasystem/MoneyTest.java
+++ src/test/java/com/example/kassasystem/MoneyTest.java
@@ -5,4 +5,20 @@ import static org.junit.Assert.*;
 
 public class MoneyTest {
 
+    @Test
+    public void testMoneyCreation() {
+        Money money = new Money(500);
+        
 }
+
+    @Test
+    public void testMoneyToString() {
+        Money money = new Money(500);
+
+    }
+
+    @Test
+    public void testMoneySetAmount() {
+        Money money = new Money(500);
+
+    }
