e7f231498c26b66d9f174401303595c1ea4d33d9
Lukas Johansson <lukasjohanssonjp@gmail.com>
2025-10-14 16:03:06 +0200

    added test files

--- src/main/java/com/example/kassasystem/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example.kassasystem;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("goodbye!");
-        System.out.println("test!");
-    }
-}
